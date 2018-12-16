d125b76607888cc3bb897326c20e12a519681b28
Alua Baikadamova <abaikad00@gmail.com>
2018-12-16 19:40:03 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -9,5 +9,8 @@ GENRES = ["Thriller", "Science Fiction", "Romance"]
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end
 end
