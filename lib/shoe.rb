2943f010ec081be85357eba70e6f4aa7058bc529
Alua Baikadamova <abaikad00@gmail.com>
2018-12-16 19:58:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,13 @@ class Shoe
   attr_accessor :color,  :size, :material, :condition
   attr_reader :brand
 BRANDS=[]
-  
+  def initialize(brand)
+    @brand = brand
+    
+    if !(BRANDS.include?( brand) ) 
+      BRANDS << brand
+    end
+  end
 
   def cobble
     self.condition = "new"
@@ -11,8 +17,7 @@ BRANDS=[]
   end
 def brand=(brand)
     @brand = brand
-    #if (BRANDS.include?( brand) ) 
-      BRANDS << brand
+    #
   #  end
  #   BRANDS & BRANDS
   end
